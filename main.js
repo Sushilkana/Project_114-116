26063d5c66604fdcd619b0aa3ccbf6fd513f909d
Sushilkana <86962755+Sushilkana@users.noreply.github.com>
2022-02-23 21:31:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function preload(){
+Nose_X = 0;
+Nose_Y = 0;
 
+function preload(){
+    Mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+    Lipstick  = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png'); 
 }
 
 function setup(){
@@ -13,6 +17,11 @@ function setup(){
 
 function draw(){
     image(video,0,0,900,500);
+    image(Mustache,Nose_X,Nose_Y,110,30);
+
+    // if (Mustache_Shot == true){
+    //     Mustache_Shot();
+    // }
 }
 
 function Save_Image(){
@@ -28,5 +37,32 @@ function gotposes(result){
      console.log(result);
      console.log("Nose x " + result[0].pose.nose.x);
      console.log("Nose y " + result[0].pose.nose.y);
+     Nose_X = result[0].pose.nose.x +72;
+     Nose_Y = result[0].pose.nose.y +30;
     }
+}
+
+function Mustache_Shot(){
+     var Mustache_Shot = true;
+    image(Mustache,Nose_X,Nose_Y,110,40);
+}
+
+function Lipstick_Shot(){
+    
+}
+
+function ClownNose_Shot(){
+    
+}
+
+function Beard_Shot(){
+    
+}
+
+function Hair_Shot(){
+    
+}
+
+function Face_Shot(){
+    
 }
